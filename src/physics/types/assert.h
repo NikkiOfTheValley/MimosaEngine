ec5972e2c2bd9e269f928ed2ffbae0a8e2e95707
ThatCodingGuy86 <75764677+ThatCodingGuy86@users.noreply.github.com>
2023-12-08 00:56:36 -0600

    small feature - Made it so `assert()` is only active in Debug mode for overhead reasons

--- src/physics/types/assert.h
+++ src/physics/types/assert.h
@@ -1,5 +1,21 @@
 #pragma once
 #include <string>
+#include <sstream>
 #include "../core/log.h"
 
-void assert(bool value, std::string failMessage);
+#ifndef NDEBUG
+#define assert(condition, message) \
+    do { \
+        _Pragma("warning(push, 0)"); \
+        if (!(condition)) { \
+            std::stringstream output; \
+            output << "Assertion `" #condition "` failed in " << __FILE__ << " line " << __LINE__ << ": " << message; \
+            Logger::getInstance().err(output.str()); \
+            Logger::getInstance().dumpLog(); \
+            exit(-200); \
+        } \
+        _Pragma("warning(pop)"); \
+    } while(false)
+#else
+#define assert(condition, message) do {} while(false)
+#endif
