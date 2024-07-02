53bfd87944ffd80c92049dd7b4cafcadf777fe83
ThatCodingGuy86 <75764677+ThatCodingGuy86@users.noreply.github.com>
2024-07-02 02:31:47 -0500

    fix - Switched to steady_clock in accurate_timer so changing the system clock doesn't break everything

--- src/core/accurate_timer.cpp
+++ src/core/accurate_timer.cpp
@@ -6,13 +6,13 @@ void BlockForNanoseconds(long long duration)
 {
 	using namespace std::chrono;
 
-	auto startTime = high_resolution_clock::now();
+	auto startTime = steady_clock::now();
 
 	// Loop until the difference between the time now and the startTime exceeds the timer duration.
 	// Yes, I know this is incredibly inefficent, but Windows timers (and std::this_thread::sleep_for) are insanely
 	// imprecise.
 
-	while (high_resolution_clock::now() - startTime < nanoseconds(duration))
+	while (steady_clock::now() - startTime < nanoseconds(duration))
 	{
 
 	}
