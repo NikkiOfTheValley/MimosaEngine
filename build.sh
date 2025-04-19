cbcd490b396ed5be949e9e9c6b6377347bb14f44
NikkiOfTheValley <nikkiofthevalley2008@gmail.com>
2025-04-19 13:50:46 -0500

    feature - added a -debug option to the build script

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-cmake -G "Unix Makefiles" -S "./" -B "build"
+if [[ "$1" == "-debug" ]]; then
+   cmake -G "Unix Makefiles" -S "./" -B "build" -D CMAKE_BUILD_TYPE=Debug
+else
+   cmake -G "Unix Makefiles" -S "./" -B "build" -D CMAKE_BUILD_TYPE=Release
+fi
+
 cmake --build "build" -j $(nproc)
